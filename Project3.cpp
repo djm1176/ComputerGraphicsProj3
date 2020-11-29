fa7f8c3ce9a9184dfe6684ada41ff2f70b185f81
Firedan1176 <Firedan1176@gmail.com>
2020-11-29 12:37:29 -0600

    Created empty Visual Studio project

---
+++ Project3.cpp
@@ -0,0 +1,15 @@
+/*==================================================================================================
+ COURSE:				  CSC 525/625
+ ASSIGNMENT:			  Project 3
+ PROGRAMMER:			  
+ LAST MODIFIED DATE:	  11/29/2020
+ DESCRIPTION:			  3D Interactive Advertisement
+ NOTE:					  
+ FILES:					  Project3.cpp, (ComputerGraphicsProj3.sln, ...)
+ IDE/COMPILER:			  Microsoft Visual Studio 2019
+ INSTRUCTION FOR COMPILATION AND EXECUTION:
+		1.		Double click on ComputerGraphicsProj3.sln	to OPEN the project
+		2.		Press Ctrl+F7 								to COMPILE
+		3.		Press Ctrl+F5 								to EXECUTE
+==================================================================================================*/
+#include <GL/freeglut.h> // include GLUT library
