52db2772ef1fca1bdb1dded7f46555f4e1fef38b
Firedan1176 <Firedan1176@gmail.com>
2020-12-02 10:08:54 -0600

    Added more scaffolding for main program

--- Project3.cpp
+++ Project3.cpp
@@ -13,3 +13,108 @@
 		3.		Press Ctrl+F5 								to EXECUTE
 ==================================================================================================*/
 #include <GL/freeglut.h> // include GLUT library
+
+void mainWindowInit();
+void helpWindowInit(); 
+
+void menuInit();
+
+void myDisplayCallback();
+void helpDisplayCallback();
+
+void keyboardCallback(unsigned char, int, int);
+void helpKeyboardCallback(unsigned char, int, int);
+
+void specialFuncCallback(int, int, int);
+void mouseCallback(int, int, int, int);
+void motionCallback(int, int);
+void reshapeCallback(int, int);
+
+constexpr int WINDOW_SIZE[]{ 800, 600 };
+constexpr int HELP_SIZE[]{ 400, 400 };
+int mainWindow, helpWindow;
+
+int main(int argc, char** argv) {
+
+	glutInit(&argc, argv); // initialization
+
+	glutInitWindowSize(WINDOW_SIZE[0], WINDOW_SIZE[1]); // specify a window size
+	glutInitWindowPosition(0, 0);						// specify a window position
+	mainWindow = glutCreateWindow("GLUT 3D Advertisement");	// create a titled window
+	mainWindowInit(); // specify some settings
+	menuInit();
+
+	glutDisplayFunc(myDisplayCallback); // register a callback
+	glutKeyboardFunc(keyboardCallback);
+	glutSpecialFunc(specialFuncCallback);
+	glutMouseFunc(mouseCallback);
+	glutMotionFunc(motionCallback);
+	glutReshapeFunc(reshapeCallback);
+
+	/*
+	glutInitWindowSize(HELP_SIZE[0], HELP_SIZE[1]);
+	glutInitWindowPosition(200, 50);
+	helpWindow = glutCreateWindow("GLUT 3D Advertisement Help");
+	helpWindowInit(); // specify some settings
+
+	glutDisplayFunc(helpDisplayCallback);
+	glutKeyboardFunc(helpKeyboardCallback);
+	*/
+
+	glutMainLoop(); // get into an infinite loop
+
+	return 1; // something wrong happened
+}
+
+//***********************************************************************************
+// Main Window and Menu Functionality
+//***********************************************************************************
+void mainWindowInit() {
+	glClearColor(1, 1, 1, 1);  // specify a background clor: white
+	gluOrtho2D(0, WINDOW_SIZE[0], WINDOW_SIZE[1], 0);  // specify a viewing area
+
+}
+
+void menuInit() {
+
+}
+
+void myDisplayCallback() {
+
+}
+
+void keyboardCallback(unsigned char, int, int) {
+
+}
+
+void specialFuncCallback(int, int, int) {
+
+}
+
+void mouseCallback(int, int, int, int) {
+
+}
+
+void motionCallback(int, int) {
+
+}
+
+void reshapeCallback(int, int) {
+
+}
+
+//***********************************************************************************
+// Help Window and Menu functionality
+//***********************************************************************************
+
+void helpWindowInit() {
+
+}
+
+void helpDisplayCallback() {
+
+}
+
+void helpKeyboardCallback(unsigned char, int, int) {
+
+}
